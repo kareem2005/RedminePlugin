21d9361a005cdda67d0be5a412278f1d22323653
djabbarov <jabbarovkareem@gmail.com>
2017-09-13 18:04:59 +0300

    added Settings and "goto Redmine" functionality

--- popup/choose_task.js
+++ popup/choose_task.js
@@ -9,9 +9,13 @@ function getApiKey() {
     return browser.storage.local.get("apiKey");
 }
 
+function onError(error) {
+    console.log(`Error: ${error}`);
+}
 
 document.addEventListener("click", function(e) {
-    // Button "Go to Redmine"
+
+  // Button "Go to Redmine"
   if (e.target.classList.contains("redmine"))
   {
       var gettingDomain = getRedmineDomain();
@@ -22,13 +26,20 @@ document.addEventListener("click", function(e) {
               url: chosenPage
           });
       }
-
-      function onError(error) {
-          console.log(`Error: ${error}`);
-      }
-
       gettingDomain.then(onGot, onError);
+      
   }
+
+    // Button "Get actual tasks"
+  if (e.target.classList.contains("tasks"))
+    {
+
+      var xhttp = new XMLHttpRequest();
+      xhttp.open("GET", "http://rm.cit-sk.ru/issues.json", false);
+      xhttp.setRequestHeader("Content-Type", "application/json");
+      xhttp.setRequestHeader("X-Redmine-API-Key", "ec80bfff50f3f6bf04249a1e0e9e9830fc2a3b27");
+
+    }
 
   // Button "Settings"
   if (e.target.classList.contains("settings"))
@@ -43,6 +54,7 @@ document.addEventListener("click", function(e) {
 
       var openingSettings = browser.runtime.openOptionsPage();
       openingSettings.then(onOpened, onError);
+      window.close();
   }
 
 });
